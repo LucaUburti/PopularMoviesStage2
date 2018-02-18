2531028a8ae3b70c34c70b9e5ba7b98de2792c46
Luca Uburti <luca.uburti@gmail.com>
2018-02-19 00:57:15 +0100

    First Commit Basic RecyclerView structure with sample urls loaded by Picasso

--- app/src/main/java/uby/luca/popularmoviesstage1/MainActivity.java
+++ app/src/main/java/uby/luca/popularmoviesstage1/MainActivity.java
@@ -2,12 +2,41 @@ package uby.luca.popularmoviesstage1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.main_rv)
+    RecyclerView mainRv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        LinearLayoutManager layoutManager = new GridLayoutManager(this, 2);
+        mainRv.setLayoutManager(layoutManager);
+
+        MovieAdapter mAdapter = new MovieAdapter(this);
+        mAdapter.add("http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg");
+        mAdapter.add("http://image.tmdb.org/t/p/w185//u351Rsqu5nd36ZpbWxIpd3CpbJW.jpg");
+        mAdapter.add("http://image.tmdb.org/t/p/w185//itdfycoMpjGWiGdjLUKMdAe9oQ5.jpg");
+        mAdapter.add("http://image.tmdb.org/t/p/w185//wVQch5MdY87uKwsoIyBLRQt7oND.jpg");
+        mAdapter.add("http://image.tmdb.org/t/p/w185//6s5SuHiUCfx7KYCqzlnem82tpYn.jpg");
+        mAdapter.add("http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg");
+        mAdapter.add("http://image.tmdb.org/t/p/w185//u351Rsqu5nd36ZpbWxIpd3CpbJW.jpg");
+        mAdapter.add("http://image.tmdb.org/t/p/w185//itdfycoMpjGWiGdjLUKMdAe9oQ5.jpg");
+        mAdapter.add("http://image.tmdb.org/t/p/w185//wVQch5MdY87uKwsoIyBLRQt7oND.jpg");
+        mAdapter.add("http://image.tmdb.org/t/p/w185//6s5SuHiUCfx7KYCqzlnem82tpYn.jpg");
+        mainRv.setAdapter(mAdapter);
+
+
     }
 }
---
+++ app/src/main/java/uby/luca/popularmoviesstage1/MovieAdapter.java
@@ -0,0 +1,63 @@
+package uby.luca.popularmoviesstage1;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+
+/**
+ * Created by uburti on 18/02/2018.
+ */
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieHolder> {
+
+    private ArrayList<String> movieList = new ArrayList<>();
+    private Context mContext;
+
+    MovieAdapter(Context context) {
+        this.mContext = context;
+    }
+
+    void add(String imageUrl) {
+        movieList.add(imageUrl);
+        notifyItemInserted(movieList.size() - 1);
+    }
+
+    @Override
+    public MovieHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_item, parent, false);
+        return new MovieHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MovieHolder holder, int position) {
+        String imageUrl = movieList.get(position);
+        Picasso.with(mContext)
+                .load(imageUrl)
+                .placeholder(R.drawable.loading_popcorn)
+                .error(R.drawable.ic_launcher_background)
+                .into(holder.movieIv);
+    }
+
+    @Override
+    public int getItemCount() {
+        return movieList.size();
+    }
+
+    class MovieHolder extends RecyclerView.ViewHolder {
+        ImageView movieIv;
+
+        MovieHolder(View view) {
+            super(view);
+            movieIv = view.findViewById(R.id.movie_iv);
+
+        }
+    }
+}
